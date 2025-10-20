444df47902ced6c9e085e4a7b75eb51b9bf5477a
tuanht2 <tuanht2@vng.com.vn>
2025-10-20 17:24:10 +0700

    feat: implement current date initialization and update user registration to use current date

--- datetime_utils.h
+++ datetime_utils.h
@@ -1,6 +1,11 @@
+extern int CURRENT_YEAR;
+extern int CURRENT_MONTH;
+extern int CURRENT_DAY;
+
 void get_expiration_date(int creation_year, int creation_month,
                          int creation_day, int& exp_year, int& exp_month,
                          int& exp_day);
 
 void input_date(int& year, int& month, int& day);
 void get_date_string(char date_str[11], int year, int month, int day);
+void initialize_current_date();
