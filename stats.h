8adc7a4a62859c01b827d9db3b774fb35571ef36
tuanht12 <thanhtuan97dn@gmail.com>
2025-11-02 22:35:12 +0700

    feat: update stats and utils functions to use defined constants for array sizes

--- stats.h
+++ stats.h
@@ -1,6 +1,35 @@
+/**
+ * @brief In ra tổng số sách trong thư viện
+ */
 void print_total_number_of_books();
+
+/**
+ * @brief In ra số lượng sách theo thể loại
+ */
 void print_number_books_by_genre();
+
+/**
+ * @brief In ra số lượng người dùng trong hệ thống
+ */
 void print_number_users();
+
+/**
+ * @brief In ra số lượng người dùng theo giới tính
+ */
 void print_number_users_by_gender();
+
+/**
+ * @brief In ra số lượng sách chưa được trả
+ */
 void print_number_unreturned_books();
+
+/**
+ * @brief In ra thông tin người dùng đang quá hạn trả sách
+ * @param user_id CMND của người dùng
+ */
+void print_late_return_user(int user_id);
+
+/**
+ * @brief In ra danh sách người dùng đang quá hạn trả sách
+ */
 void print_current_late_users();
