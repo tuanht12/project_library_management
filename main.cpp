6e3bed2c1bec2c2969f608712363114fa0bde267
tuanht12 <thanhtuan97dn@gmail.com>
2025-12-01 22:33:18 +0700

    refactor: improve comments for clarity in main function

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@
 #include "users.h"
 #include "utils.h"
 int main() {
-    // Đọc dữ liệu sách từ file CSV
+    // Đọc dữ liệu từ file CSV
     int num_current_books = read_books_from_csv();
     int num_current_users = read_users_from_csv();
     int num_current_borrows = read_borrows_from_csv();
-    if (SHOULD_INIT_DATA_FOR_TESTING) {
+    if (SHOULD_INIT_DATA_FOR_TESTING &&
+        (num_current_books == 0 || num_current_users == 0 ||
+         num_current_borrows == 0)) {
         printf("Khởi tạo dữ liệu mẫu cho mục đích testing...\n");
         if (!num_current_books) initialize_test_books();
         if (!num_current_users) initialize_test_users();
@@ -21,7 +23,7 @@ int main() {
     }
     initialize_current_date();
     display_main_menu();
-    // Lưu dữ liệu sách vào file CSV khi thoát chương trình
+    // Lưu dữ liệu vào file CSV khi thoát chương trình
     write_books_to_csv();
     write_users_to_csv();
     write_borrows_to_csv();
